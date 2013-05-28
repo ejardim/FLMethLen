8f07d2be952de21e68fa2638e58cd90c1fa34bf0
laurie <lauriekell@gmail.com>
2013-05-28 09:22:23 +0200

    random sampling methods

---
+++ R/lk-methods.R
@@ -0,0 +1,60 @@
+#' resample
+#' 
+#' @description 
+#' Resamples from a distribution to generate a frequency table
+#'       
+#' @param object an \code{FLQuant}
+#' @param dim dimensions of \code{FLQuant} to sample within, i.e. if equal to \code{2} then sampling is within \code{year}
+#' @param size sum of frequency disribution
+#' @param replace sample with replacement?, defaults to \code{FALSE} only useful if values in \code{object} are integers
+#' @params ...
+#' @return a \code{FLQuant} with simulated frequency distribution
+#' @export
+#' @docType methods
+#' @rdname lk-methods
+#' 
+#' @examples
+#' \dontrun{
+#'   library(FLCore)
+#'   data(ple4sex)
+#'   
+#'   apply(resample(catch.n(ple4sex)[,1:4],2,  100),2,  sum)
+#'   apply(resample(catch.n(ple4sex)[,1:4],2:3,100),2:3,sum)
+#'   apply(resample(catch.n(ple4sex)[,1:4],1:2,100),1:2,sum)
+#'   }
+
+setGeneric('resample',   function(x,...) 
+  standardGeneric('resample'))
+
+setMethod('resample',  signature(x='FLQuant'), 
+    function(x,dim=2:6,size,replace=T,...) {
+      
+      resampleFn=function(x,size,replace=T,...){
+        
+        sampleFn= function(x, ...) x[sample.int(length(x),...)]
+        
+        y  =c(x)
+        nms=seq(length(y))
+        
+        res=sampleFn(nms,size,replace,prob=y,...)
+        res=factor(res,levels=nms)
+        
+        summary(res)}
+      
+      res=apply(x,dim,resampleFn,size=size,replace=replace,...)
+        
+        ## rearranges array to get dims in FLQuant correct
+        d=seq(length(dim(x)))
+        i=c(d[!(d %in% dim)],dim)
+        names(i)=d
+        o=as.numeric(names(i[order(i)]))
+        
+        dmns=dimnames(x)[i]
+        res=apply(x,dim,resampleFn,size=100)
+        
+        res=array(c(res),unlist(lapply(dmns,length)),dimnames=dmns)
+        
+        as.FLQuant(aperm(res,o))})
+  
+  
+
